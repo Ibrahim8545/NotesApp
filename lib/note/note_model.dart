d78a5e875ea0259ea3ff1569f8965e9422f26156
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-03-14 00:45:33 +0200

    add hive oackege to project

---
+++ lib/note/note_model.dart
@@ -0,0 +1,12 @@
+class NotesModelButton
+{
+  final String title,subTitle,date;
+  final int color;
+
+  NotesModelButton
+  ({required this.title,
+     required this.subTitle, 
+     required this.date, 
+     required this.color
+  });
+}
